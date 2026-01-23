6edce53c7739d362b8799ccca7ddd525b168e4ac
llins <llinsomoudu@gmail.com>
2026-01-23 13:02:30 +0100

    portfoliomodule

--- src/portfolio/portfolio.module.ts
+++ src/portfolio/portfolio.module.ts
@@ -3,16 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PortfolioService } from './portfolio.service';
 import { PortfolioController } from './portfolio.controller';
 import { Trade } from '../trades/entities/trade.entity';
+import { Position } from './entities/position.entity';
 import { User } from '../users/entities/user.entity';
 import { PriceService } from '../shared/price.service';
 import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-      TypeOrmModule.forFeature([Trade, User]),
-      CacheModule.register(),
+    TypeOrmModule.forFeature([Trade, Position, User]),
+    CacheModule.register(),
   ],
   controllers: [PortfolioController],
   providers: [PortfolioService, PriceService],
+  exports: [PortfolioService],
 })
 export class PortfolioModule {}
